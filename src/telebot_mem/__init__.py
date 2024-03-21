f25141bcc578cf1fe46d71b493f121620e73db9d
Wei Li <wei@chromium.org>
2024-03-21 00:30:56 -0700

    First working version

--- src/telebot_mem/__init__.py
+++ src/telebot_mem/__init__.py
@@ -4,5 +4,10 @@ from telebot import types
 
 # Override the de_json method for messages to also save a copy of input strings
 types.Message._de_json = types.Message.de_json
-types.Message.de_json = lambda self, json_string: self._de_json(json_string) and self.__setattr__("json", json_string)
+def de_json(json_string):
+    message = types.Message._de_json(json_string)
+    if message:
+        message.__setattr__("json", json_string)
+    return message
+types.Message.de_json = de_json
 
--- src/telebot_mem/mongo_db.py
+++ src/telebot_mem/mongo_db.py
@@ -1,15 +1,19 @@
+import json
+
 from telebot_mem.db import MemDB
 
 from pymongo import MongoClient
 from telebot.types import Message
 
 class MongoDB(MemDB):
-    def __init__(self, *args, **kwargs):
-        self.client = MongoClient(*args, **kwargs)
+    def __init__(self, connecion_string, db_name):
+        self.client = MongoClient(connecion_string)[db_name]
 
     def insert_message(self, message: Message):
-        
-        self.client.messages.insert_one(message)
+        if message and message.json:
+            obj = message.json
+            obj.update({"_id": message.message_id})
+            self.client.messages.insert_one(obj)
 
     def get_message(self, message_id):
         return self.client.messages.find_one({"_id": message_id})
@@ -21,6 +25,7 @@ class MongoDB(MemDB):
     # decorator methods
     def memorize(self, func):
         def wrapper(*args, **kwargs):
-            self.insert_message(func(*args, **kwargs))
+            self.insert_message(args[0] if args else kwargs["message"])
+            func(*args, **kwargs)
 
         return wrapper
